0dbc17941f5905ba1caff972360cd340bf4d5be7
Yoshihiro Tanaka <contact@cordea.jp>
2021-09-04 10:43:57 +0900

    Initial Commit

---
+++ Snowflake/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Snowflake
+//
+//  Created by Yoshihiro Tanaka on 9/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Snowflake/SnowflakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowflakeApp.swift
+//  Snowflake
+//
+//  Created by Yoshihiro Tanaka on 9/4/21.
+//
+
+import SwiftUI
+
+@main
+struct SnowflakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
